f26773a67c71f9306677c1bb2deb76e0a94e3ae3
panjing <panjing.binary@gmail.com>
2024-01-03 22:52:15 +0800

    feat: Add Duration component

---
+++ src/components/Duration.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  start: string
+  end?: string
+}
+
+const { start, end } = defineProps<Props>()
+</script>
+
+<template>
+  <span v-if="!end">{{ start }}</span>
+  <span v-else>
+    <span>{{ start }}</span>
+    <span> - </span>
+    <span>{{ end }}</span>
+  </span>
+</template>
