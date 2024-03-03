7456dd95ec22bef677ad8d72b82a06aa874f317d
panjing <panjing.binary@gmail.com>
2024-03-04 01:22:38 +0800

    chore: cleanup

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    Content: typeof import('./src/components/Content.vue')['default']
     Duration: typeof import('./src/components/Duration.vue')['default']
     Header: typeof import('./src/components/Header.vue')['default']
     Highlights: typeof import('./src/components/Highlights.vue')['default']
